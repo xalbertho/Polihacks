eb967bea65a6c56661b2397bc62f93507e6001e3
Alberto <129074553+xalbertho@users.noreply.github.com>
2025-11-23 13:45:42 -0600

    Modify camera pin configuration in CameraWebServer
    Updated camera pin assignments for ESP32.

--- device_esp32/CameraWebServer.ino
+++ device_esp32/CameraWebServer.ino
@@ -127,14 +127,13 @@ void setup() {
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
   config.pin_d1 = Y3_GPIO_NUM;
   config.pin_d2 = Y4_GPIO_NUM;
   config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y4_GPIO_NUM;
-  config.pin_d5 = Y5_GPIO_NUM;
-  config.pin_d6 = Y6_GPIO_NUM;
-  config.pin_d7 = Y7_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
   config.pin_xclk = XCLK_GPIO_NUM;
   config.pin_pclk = PCLK_GPIO_NUM;
   config.pin_vsync = VSYNC_GPIO_NUM;
